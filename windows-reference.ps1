626a8392282b12880291afb40c330d346a2bbc32
PauloRodriguesPH <127219315+PauloRodriguesPH@users.noreply.github.com>
2026-02-11 23:49:50 -0300

    Update windows-reference.ps1

--- windows-reference.ps1
+++ windows-reference.ps1
@@ -1,2 +1,13 @@
+#####################################################
+# GUIA DE REFERÊNCIA DE COMANDOS WINDOWS POWERSHELL #
+#####################################################
+
+<#
+# Autor: Paulo Henrique da Silva Motta Rodrigues
+# Testado em: Windows Server
+# Data do teste: 06/01/2026
+# Uso: este arquivo é um catálogo; copie e rode os comandos conforme necessidade.
+#>
+
 #Trocar timezone para UTC -3:00 Brasilia
 Set-TimeZone -Id "E. South America Standard Time"
