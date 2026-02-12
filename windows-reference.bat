bdc0e81110514bdab79c2381f07beb1a3dec4399
PauloRodriguesPH <127219315+PauloRodriguesPH@users.noreply.github.com>
2026-02-11 23:49:13 -0300

    Update windows-reference.bat

--- windows-reference.bat
+++ windows-reference.bat
@@ -1,3 +1,14 @@
+##############################################
+# GUIA DE REFERÊNCIA DE COMANDOS WINDOWS CMD #
+##############################################
+
+<#
+# Autor: Paulo Henrique da Silva Motta Rodrigues
+# Testado em: Windows Server
+# Data do teste: 06/01/2026
+# Uso: este arquivo é um catálogo; copie e rode os comandos conforme necessidade.
+#>
+
 REM Desabilita a interface de rede
 wmic path win32_networkadapter where NetConnectionID="NIC_LAN" call disable
 
